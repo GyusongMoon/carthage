34eb50b207f4d0302e1292ee724372058e3747e5
grabity <kyusong12@grabity.org>
2019-11-25 17:19:09 +0900

    first commit

---
+++ Carthage/Carthage.h
@@ -0,0 +1,19 @@
+//
+//  Carthage.h
+//  Carthage
+//
+//  Created by grabity on 25/11/2019.
+//  Copyright © 2019 grabity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Carthage.
+FOUNDATION_EXPORT double CarthageVersionNumber;
+
+//! Project version string for Carthage.
+FOUNDATION_EXPORT const unsigned char CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Carthage/PublicHeader.h>
+
+
