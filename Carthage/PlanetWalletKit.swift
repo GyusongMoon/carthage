2d59d27dea996b9f9af0e49b330f075132cb60c1
grabity <kyusong12@grabity.org>
2019-11-25 17:24:54 +0900

    add PlanetWalletKit

---
+++ Carthage/PlanetWalletKit.swift
@@ -0,0 +1,15 @@
+//
+//  PlanetWalletKit.swift
+//  Carthage
+//
+//  Created by grabity on 25/11/2019.
+//  Copyright © 2019 grabity. All rights reserved.
+//
+
+import Foundation
+
+class PlanetWalletKit {
+    init() {
+        print("init PlanetWalletKit")
+    }
+}
